370c3240d72cb3c383007f1bc5a9560f19d9e628
Greg Leaver <greg.leaver@gmail.com>
2012-06-21 17:52:45 -0400

    Fix sorting so that it handles documents with no year.

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,13 @@
 module CoursesHelper
+  def sort_by_year(documents)
+    documents.sort do |a,b|
+      if a.year.nil? then
+        1
+      elsif b.year.nil? then
+        -1
+      else
+        b.year <=> a.year
+      end
+    end
+  end
 end
